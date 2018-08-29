c18d790f03a92dbb10bd3404e53922a95b35adac
eduhlira2 <eduardu.silva@hotmail.com>
2018-08-29 18:34:32 -0400

    Projeto Atual

---
+++ App MaterialManaosTech/Assets/AnimationTimer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTimer : MonoBehaviour{
+
+	public float TimeRemaining;
+	public float TimeRemaining2;
+	private bool TimeIsOver=false;
+	public GameObject professor;
+	private Animator professorAnim;
+
+	// Use this for initialization
+	void Start () {
+		TimeRemaining2 = TimeRemaining;
+		professorAnim=professor.GetComponent<Animator> ();
+		professorAnim.SetBool ("Falar", true);
+		professorAnim.SetBool ("Tchau", false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		/*if (DefaultTrackableEventHandler.zerar == true) {
+			ZerarTudo ();
+		}*/
+		if (TimeIsOver == false) {
+			professorAnim.SetBool ("Falar", true);
+			professorAnim.SetBool ("Tchau", false);
+		} else {
+			professorAnim.SetBool ("Falar", false);
+			professorAnim.SetBool ("Tchau", true);
+		}
+
+		if (TimeIsOver == false) {
+			TimeRemaining2 -= Time.deltaTime;
+			if (TimeRemaining2 <= 1.0f) {
+				TimeIsOver = true;
+			}
+		}
+	}
+
+	void ZerarTudo(){
+		TimeRemaining2 = TimeRemaining;
+		TimeIsOver = false;
+		professorAnim.SetBool ("Falar", true);
+		professorAnim.SetBool ("Tchau", false);
+		//DefaultTrackableEventHandler.zerar = false;
+	}
+}
---
+++ App MaterialManaosTech/Assets/Scripts/PlayerMovie.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PlayerMovie : MonoBehaviour {
+
+    //public Button playButton;
+    
+    public VideoPlayer player;
+    public GameObject playButton;
+    public GameObject PauseButtonInvisible;
+
+    public GameObject fullscreen;
+    public GameObject MainObject;
+    public VideoPlayer FullScreenPlayer;
+    
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+    public void Play()
+    {
+        player.Play();
+        playButton.SetActive(false);
+        PauseButtonInvisible.SetActive(true);
+              
+    }
+
+    public void Pause()
+    {
+        player.Pause();
+        PauseButtonInvisible.SetActive(false);
+        playButton.SetActive(true);
+    }
+
+    public void FullScreen()
+    {
+        fullscreen.SetActive(true);
+        FullScreenPlayer.frame = player.frame;
+        MainObject.SetActive(false);
+    }
+
+    public void FullScreenExit()
+    {
+        fullscreen.SetActive(false);
+    }
+  }
